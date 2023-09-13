e4b91235c56d4323ae2bde579e0695f81dbf6f57
cazlito <142947671+cazlito@users.noreply.github.com>
2023-09-12 15:01:26 -1000

    did part 8

--- HackwichTwo_9.5.23/ViewController.swift
+++ HackwichTwo_9.5.23/ViewController.swift
@@ -8,15 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //Part 8: declare 4 string variable
+    var myFirstVariable = "hello i am string 1"
+    var mySecondVariable = " hi im string 2"
+    var myThirdVariable = ""
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let myThirdVariable = myFirstVariable + mySecondVariable
+        print(myThirdVariable)
     }
 
+    @IBAction func anotherButton(_ sender: Any) {
+        print(myThirdVariable)
+    }
+    
     @IBAction func pressMeButton(_ sender: Any) {
         print("Hello world")
     }
 }
+
 
 
